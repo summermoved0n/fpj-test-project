58f1bda1a479473a6912f95c4c9314d50cac63a3
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-08-25 22:15:58 -0400

    mod 1-1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-const answer = prompt('Who are you?');
-console.log(answer);
-console.log(typeof answer);
+// const answer = prompt('Who are you?');
+// console.log(answer);
 
+// let figure = 5;
+// figure = figure + 5;
+// console.log(figure)
+
+// console.log(Math.random() * (565 - 1) + 1);
 
 
 
