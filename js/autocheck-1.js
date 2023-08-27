f2f9216080985e4a8cc6ad9736f401e8b905243b
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-08-26 23:47:42 -0400

    hw1-1

---
+++ js/autocheck-1.js
@@ -0,0 +1,42 @@
+// ?------- GOIT HW-1 -------
+
+// ! Сумма рядків
+
+// let a = 6;
+// let b = false;
+// let c = "12";
+// let d = 1;
+// let result = a + d + c + b;
+// console.log(result);
+
+// ! Шаблонний рядок
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message)
+
+// ! Problem #1
+
+// let pricePerDroid = 800;
+// let orderedQuantity = 6;
+// let deliveryFee = 50;
+// let totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// ! Функція sayHi
+
+// function sayHi() {
+//   console.log("Hello, this is my first function!");
+// }
+// sayHi();
+
+// ! Функція add
+
+// function add(a, b, c) {
+// console.log(`Addition result equals ${a + b + c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
