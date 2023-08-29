4ac8640fad0d45dfedc3f1988f13129d47485319
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-08-28 23:34:11 -0400

    if....else

--- js/autocheck-1.js
+++ js/autocheck-1.js
@@ -40,6 +40,43 @@
 // add(10, 20, 30);
 // add(5, 10, 15);
 
-// ! Лекція 1-2
+// ! Лекція 1-2 (логічні оператори)
+
+// console.log(true && 3)
+// console.log(false && 3)
+// console.log(true && 3 && 'kiwi')
+// console.log(true && '' && 'kiwi')
+// console.log(true && ' ' && 'kiwi')
+// console.log(true && 0 && 'kiwi')
+// console.log(true || 3)
+// console.log(true || 3 || 4)
+// console.log(true || false || 4)
+// console.log(false || true || 4)
+// console.log(null || 2 || undefined)
+// console.log(null || 2 || undefined)
+// console.log((1 && null && 2) > 0)
+// console.log(null || (1 && 2) || 4)
+// console.log(null || (null && 2) || 4)
+
+// ! Лекція 1-2 (розгалудження)
+const quation1 = confirm('Do you like girls?')
+const quation2 = Number(prompt('How old are you?'))
+console.log(quation2)
+
+if (quation2 >= 18) {
+  console.log(alert('Hello big guy!'))
+} else {
+  console.log(alert('Small dick!'))
+}
+
+const quation3 = Number(prompt('What girl`s age do you like?'))
+console.log(quation3)
+
+
+
+
+
+
+
 
 
