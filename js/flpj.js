1ebd949112dd0c14cebf3bd95447d28a8bd4931c
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-09-10 16:58:14 -0400

    flpj - typeof

---
+++ js/flpj.js
@@ -0,0 +1,34 @@
+// ! Переменные в JAVASCRIPT. Константы. Ключевые слова LET CONST VAR. Уроки JAVASCRIPT с нуля 2020
+
+// let eyeColot
+// let user
+// let userName = "Vasya"
+// user = userName
+// console.log(user);
+
+//! Типы данных JAVASCRIPT. Null String Undefined Boolean Number и другие. Уроки JAVASCRIPT с нуля 2020
+
+// ? homework
+
+// // * V1 *
+
+// let userAge = 36
+// let userInfo = 'Frilanser ${userAge}'
+// console.log(userInfo);
+
+// // * V2 *
+
+// let userHeight = 145 / 0
+// console.log(userHeight)
+
+// // * V3 *
+
+// let userName
+// console.log(typeof userName)
+
+// // * V4 *
+
+// let userSize = '45' / '8'
+// console.log(typeof userSize)
+
+// !
